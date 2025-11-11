3b73c1ae3a305444679c019ed8a8afaf45f78387
Shivam Goel <shivamkgjj2005@gmail.com>
2025-11-11 21:28:36 +0530

    Final Fix

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {AuthProvider} from './components/AuthContext.jsx'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
+  
     <App />
-    </AuthProvider>
+    
   
 )
